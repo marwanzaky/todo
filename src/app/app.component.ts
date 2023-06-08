07c065dcf0468f9d3c6bf709f8dd1c7fbfdb6d90
Marwan Zaky <marouaneezzaky@gmail.com>
2023-06-08 04:30:01 +0100

    UI & UX polishing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,23 @@ import { Task } from './task';
 })
 export class AppComponent {
   title = 'todo';
-  tasks: Task[] = [];
+  tasks: Task[] = [
+    {
+      name: 'Buy groceries: Milk, eggs, bread, and fruits.',
+      description: 'Ensure you have essential food items by purchasing milk, eggs, bread, and a variety of fruits for the week.',
+      completed: false
+    },
+    {
+      name: 'Finish report for the meeting with the client.',
+      description: 'Compile and complete the report that needs to be presented during the upcoming meeting with the client.',
+      completed: false
+    },
+    {
+      name: 'Call the doctor\'s office to schedule an appointment.',
+      description: 'Contact the doctor\'s office to book an appointment for a medical check- up or consultation.',
+      completed: false
+    }
+  ];
   selectedTask?: Task;
 
   addTaskForm = new FormGroup({
